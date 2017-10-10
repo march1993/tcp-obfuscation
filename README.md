a96e3b62099d27d45530400544b166d2514ecb05
march1993 <march511@gmail.com>
2017-10-10 20:28:38 +0200

    todo: encode & decode

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 This crazy project hides entire content in an IPv4 or IPv6 packet.
 
+# todo
+
+Implementation for ipv6.
+
--- tcp-obfuscation-service.c
+++ tcp-obfuscation-service.c
@@ -6,29 +6,79 @@ struct rule rules[] =
 ;
 
 
-void encode (unsigned char * begin, unsigned short length) {
-	printk("length: %d\n", length);
+void encode (unsigned char * buffer, unsigned short length) {
+	printk("encode: length: %d\n", length);
 }
 
-void decode (unsigned char * begin, unsigned short length) {
-
+void decode (unsigned char * buffer, unsigned short length) {
+	printk("decode: length: %d\n", length);
 }
-
 
 
 unsigned int tcp_obfuscation_service_outgoing (
 	void * priv,
 	struct sk_buff * skb,
-	const struct nf_hook_state * state)
-{
+	const struct nf_hook_state * state) {
+
+	struct iphdr * ipv4_header;
+	struct ipv6hdr * ipv6_header;
+	// protocol family
+	u_int8_t pf;
+	const unsigned n_rules = sizeof(rules) / sizeof(struct rule);
+	unsigned i;
+
+	pf = state->pf;
+
+	ipv4_header = ip_hdr(skb);
+	ipv6_header = ipv6_hdr(skb);
+
+	for (i = 0; i < n_rules; i ++) {
+
+		struct rule * r = rules + i;
+
+		// rule's protocol should be equal to packet's protocol
+		if (r->protocol != pf) {
+
+			continue;
+
+		}
+
+
+		// address should match
+		if (pf == PF_INET && r->peer_ipv4._in4 == ipv4_header->daddr) {
+
+			unsigned short
+				iph_len = ipv4_header->ihl * 4,
+				tot_len = __be16_to_cpu(ipv4_header->tot_len),
+				payload_len = tot_len - iph_len;
+
+			unsigned char * payload = ((unsigned char *) ipv4_header) + iph_len;
+
+			encode(payload, payload_len);
+
+			return NF_ACCEPT;
+
+		} else
+		if (pf == PF_INET6 && memcmp(&r->peer_ipv6._in6, &ipv6_header->saddr, sizeof(struct in6_addr)) == 0) {
+
+			return NF_ACCEPT;
+
+		}
+
+	}
+
 	return NF_ACCEPT;
+
 }
+
+
+
 
 unsigned int tcp_obfuscation_service_incoming (
 	void * priv,
 	struct sk_buff * skb,
-	const struct nf_hook_state * state)	
-{
+	const struct nf_hook_state * state) {
+
 	struct iphdr * ipv4_header;
 	struct ipv6hdr * ipv6_header;
 	// protocol family
@@ -60,15 +110,15 @@ unsigned int tcp_obfuscation_service_incoming (
 				iph_len = ipv4_header->ihl * 4,
 				tot_len = __be16_to_cpu(ipv4_header->tot_len),
 				payload_len = tot_len - iph_len;
-			
+
 			unsigned char * payload = ((unsigned char *) ipv4_header) + iph_len;
-			
-			encode(payload, payload_len);
+
+			decode(payload, payload_len);
 
 			return NF_ACCEPT;
 
 		} else
-	       	if (pf == PF_INET6 && memcmp(&r->peer_ipv6._in6, &ipv6_header->saddr, sizeof(struct in6_addr)) == 0) {
+		if (pf == PF_INET6 && memcmp(&r->peer_ipv6._in6, &ipv6_header->saddr, sizeof(struct in6_addr)) == 0) {
 
 			return NF_ACCEPT;
 
@@ -77,8 +127,14 @@ unsigned int tcp_obfuscation_service_incoming (
 	}
 
 	return NF_ACCEPT;
+
 }
+
+
+
 
 
 EXPORT_SYMBOL(tcp_obfuscation_service_incoming);
 EXPORT_SYMBOL(tcp_obfuscation_service_outgoing);
+
+
